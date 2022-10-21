e7af7c9a9554bf458ac8e041f80ec4448e23edfc
Marcelo Morales <morales.374@buckeyemail.osu.edu>
2022-10-20 23:19:39 -0400

    Adding types to slugs and filtering by them

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,45 +16,59 @@ type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 const Home: NextPage<PageProps> = ({ posts }) => {
   const cardBg = useColorModeValue("gray.200", "gray.700");
   const imgBg = useColorModeValue("gray.50", "gray.500");
+
+  const types = ["Classes", "Research", "Fun"];
+
   return (
     <PageLayout>
-      <Heading margin={8} as="h3">
+      <Heading margin={8} as="h2">
         Notes and Resources
       </Heading>
-      <Flex flexDir="row" p="8" gap="4" flexWrap="wrap">
-        {posts.map((post) => {
-          return (
-            <Link key={post.slug} href={`/posts/${post.slug}`}>
-              <Box
-                w="360px"
-                background={cardBg}
-                rounded="md"
-                overflow="hidden"
-                cursor="pointer"
-                shadow="lg"
-                transition="all 200ms ease-in-out"
-                _hover={{
-                  transform: "scale(1.025)",
-                }}
-              >
-                <Img
-                  src={post.banner}
-                  w="360px"
-                  h="200px"
-                  objectFit="contain"
-                  bg={imgBg}
-                />
-                <Box p="2">
-                  <Heading as="h3" size="md">
-                    {post.title}
-                  </Heading>
-                  <Text>{post.author}</Text>
-                </Box>
-              </Box>
-            </Link>
-          );
-        })}
-      </Flex>
+      {types.map((type) => {
+        return (
+          <>
+            <Heading marginLeft={8} as="h3">
+              {type}
+            </Heading>
+            <Flex flexDir="row" p="8" gap="4" flexWrap="wrap">
+              {posts
+                .filter((post) => post.type === type)
+                .map((post) => {
+                  return (
+                    <Link key={post.slug} href={`/posts/${post.slug}`}>
+                      <Box
+                        w="360px"
+                        background={cardBg}
+                        rounded="md"
+                        overflow="hidden"
+                        cursor="pointer"
+                        shadow="lg"
+                        transition="all 200ms ease-in-out"
+                        _hover={{
+                          transform: "scale(1.025)",
+                        }}
+                      >
+                        <Img
+                          src={post.banner}
+                          w="360px"
+                          h="200px"
+                          objectFit="contain"
+                          bg={imgBg}
+                        />
+                        <Box p="2">
+                          <Heading as="h3" size="md">
+                            {post.title}
+                          </Heading>
+                          <Text>{post.author}</Text>
+                        </Box>
+                      </Box>
+                    </Link>
+                  );
+                })}
+            </Flex>
+          </>
+        );
+      })}
     </PageLayout>
   );
 };
