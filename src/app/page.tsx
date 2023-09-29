da3fea5c624649049067bc46b92afdfb3cc16c55
Marcelo Morales <morales.374@osu.edu>
2023-09-29 14:44:49 -0400

    Tempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-
-
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='min-h-screen flex items-center justify-center '>
-        <div className=' p-8 rounded-lg shadow-md text-center'>
-          <h1 className='text-3xl font-semibold text-blue-500 mb-4'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="min-h-screen flex items-center justify-center ">
+        <div className=" p-8 rounded-lg shadow-md text-center">
+          <h1 className="text-3xl font-semibold text-blue-500 mb-4">
             Under Construction
           </h1>
-          <p className='text-gray-600'>
+          <p className="text-gray-600">
             I'm doing some refactoring! Please check back later.
-
-
           </p>
         </div>
+
+
+
       </div>
     </main>
   );
